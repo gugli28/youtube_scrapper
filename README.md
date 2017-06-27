9b2a02c4b76a8516c55296a38e2c53878749eaf9
gugli <princebgt28@gmail.com>
2017-06-27 18:26:58 +0530

    scrapping youtube

---
+++ README.md
@@ -0,0 +1,8 @@
+File(maha.py) downloads best audio and video from using the list of links in the html file.
+- The links are taken from the source pace of the html page
+- Just checkout the remaining file and you will get the idea
+
+##to run
+`python maha.py`
+
+#JUST INSTALL `youtube-dl` in your system
---
+++ maha.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+import os
+import re
+from bs4 import BeautifulSoup
+# //regex
+# find_http = re.compile("^http")
+# opening file
+# fileObject = open("/home/prince/Documents/documents/scripts_py/rajya_sabha.txt" ,"r")
+fileObject = open("/home/gugli/Documents/script_py/maha.html" ,"r")
+
+soup = BeautifulSoup(fileObject.read())
+##finding tag a in the page source
+a_tag = soup.find_all("a")
+
+href_list = []
+for item in a_tag:
+	href_list += [item['href']]
+# print href_list
+
+# dir_name = raw_input("dirname ")
+# name = "mkdir" + dir_name
+os.system("mkdir rama_vid")
+os.chdir("/home/gugli/Documents/script_py/rama_vid")
+# os.chdir("/home/prince/Documents/documents/scripts_py/others")
+
+os.system("pwd")
+count = 1
+for line in href_list:
+# 	if find_http.search(line):
+	url = "https://www.youtube.com"+ line.rstrip()
+	# b = "youtube-dl -f 18 " + url
+	b = "youtube-dl -f bestvideo+bestaudio " + url
+
+	print "COMMAND " , b,'\n'
+	# print url,'\n'
+	os.system(b)
+	print "Done " , count , '\n',b
+
+print "wtf"
